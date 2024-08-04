aeb30eaf469ade44f963a3553b33cb36a7c94d8d
ReneeF <reneeffs@gmail.com>
2024-08-04 23:50:01 +0300

    personalMovieDB

---
+++ Js/main.js
@@ -0,0 +1,22 @@
+const numberOfFilms = +prompt('How many films do you watch?', '');
+
+
+
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {
+
+  },
+  actors: {},
+  genres: [],
+  privat: false
+};
+
+const yourLastWatchMovie = prompt('What the last movie do you watch?', '');
+const yourScore = prompt('Your score', '');
+
+personalMovieDB.movies[yourLastWatchMovie] = yourScore
+
+console.log(personalMovieDB);
+
